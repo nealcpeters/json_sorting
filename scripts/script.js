b2655da0af0a903309e660ea1e29fad354aac795
Neal Peters <nealcpeters@gmail.com>
2014-05-15 19:48:08 -0500

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+$.getJSON("data/data.json",
+function (data) {
+    var tr;
+    for (var i = 0; i < data.length; i++) {
+        tr = $('<tr/>');
+        tr.append("<td>" + data[i].carrierName + "</td>");
+        tr.append("<td>" + data[i].planName + "</td>");
+        tr.append("<td>" + data[i].copay + "</td>");
+        tr.append("<td>" + data[i].premium + "</td>");
+        tr.append("<td>" + data[i].annualLimit + "</td>");
+        $('table').append(tr);
+    }
+});
