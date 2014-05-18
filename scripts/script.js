5b3e9c440a6ef74f287c2531a8e8f014447dc85b
Neal Peters <nealcpeters@gmail.com>
2014-05-18 17:52:17 -0500

    remove unnecessary html and javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,3 @@
-// $.getJSON("data/data.json",
-//     function (data) {
-//         data.sort(function(a, b){
-//             return a.carrierName > b.carrierName;
-//         });
-
-//     var headers = Object.keys(data[1]);
-
-//     for(var i=0;i<data.length;i++){
-//         $('thead').append("<th>" + headers[i] + "</th>");        
-//         var obj = data[i];
-//         for(var key in obj){
-//             $('table').append('<tr></tr>');
-//             $($('tr')[i]).append("<td>" + obj[key] + "</td>");
-//         }
-//     }
-
-
-//     // for (var i = 0; i < data.length; i++) {
-//     //     var test = 0;
-//     //     tr = $('<tr/>');
-//     //     th = $('thead');
-//     //     th.append("<th>" + headers[i] + "</th>");
-//     //     tr.append("<td>" + data[i].carrierName + "</td>");
-//     //     tr.append("<td>" + data[i].planName + "</td>");
-//     //     tr.append("<td>" + data[i].copay + "</td>");
-//     //     tr.append("<td>" + data[i].premium + "</td>");
-//     //     tr.append("<td>" + data[i].annualLimit + "</td>");
-//     //     $('table').append(tr);
-//     //     test++;
-//     // }
-// });
-
 function DataParser(filePath){
     var getData = function(fPath, callBack){
         $.getJSON(fPath, callBack)
@@ -51,7 +18,7 @@ function DataParser(filePath){
         $('thead').append("<th>" + headers[i] + "</th>");        
         var obj = data[i];
         for(var key in obj){
-            $('table').append('<tr></tr>');
+            $('table').append('<tr/>');
             $($('tr')[i]).append("<td>" + obj[key] + "</td>");
         }
     }
